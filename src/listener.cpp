ce72a07aa22fe0007ad9a64d0b45db69bff18e8e
Umut Sevdi <sevdiumut@gmail.com>
2023-04-05 18:24:55 +0000

    finished collector example

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,25 +1,61 @@
+/******************************************************************************
+ * File: basics/listener.cpp
+ *
+ * Author: Umut Sevdi
+ * Created: 04/05/23
+ * Description: In this example a listener with the frequency of one listens
+ * the topic and pushes all incoming messages into a stack.
+ * - Every 10 seconds collector captures all elements in the stack, and pushes
+ *  all of them in JSON format.
+ * *****************************************************************************/
 #define __ROS_VERSION__ "4"
 #include <ros/ros.h>
 #include <iostream>
 #include <std_msgs/String.h>
+#include <thread>
 
-#include <functional>
-void default_callback(const std_msgs::String::ConstPtr& msg);
+ /**
+  * collect - Displays the incoming message and pushes into a stack
+ */
+void collect(const std_msgs::String::ConstPtr& msg);
+
+std::vector<std::string> stack;
 
 int main(int argc, char** argv) {
-    /* code */
-    std::cout << "/* message */" __ROS_VERSION__ << std::endl;
     ros::init(argc, argv, "basics_listener");
-    std::cout << "/* <> */" << std::endl;
+    ROS_INFO("Started listener thread");
     ros::NodeHandle handler;
 
     ros::Subscriber subscriber = handler
-        .subscribe<std_msgs::String>("topic", 1000, default_callback);
-    ros::Rate loop_rate(1);
+        .subscribe<std_msgs::String>("topic", 1000, collect);
+    ros::Publisher collector = handler.advertise<std_msgs::String>("collector", 2000);
+
+    std::thread t([collector]() {
+        ROS_INFO("Started collector thread");
+        ros::Rate loop_rate(0.1);
+        while (ros::ok()) {
+            std::stringstream s;
+            s << "[";
+            while (!stack.empty()) {
+                std::string& msg = stack.back();
+                s << msg << ", ";
+                stack.pop_back();
+            }
+            s << "\n]";
+            std_msgs::String m;
+            m.data = s.str();
+            if (m.data != "[\n]") {
+                collector.publish(m);
+            }
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+        });
     ros::spin();
-
 }
 
-void default_callback(const std_msgs::String::ConstPtr& msg) {
-    std::cout << "LISTENER " << ros::this_node::getName() << ":: " + msg->data << std::endl;
+void collect(const std_msgs::String::ConstPtr& msg) {
+    stack.push_back(msg->data);
+    std::cout << "LISTENER " << msg->data << stack.empty() << std::endl;
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,35 @@
+/******************************************************************************
+ * File: basics/main.cpp
+ *
+ * Author: Umut Sevdi
+ * Created: 04/05/23
+ * Description: Simple publisher example, shares a string message in constant
+ * rate
+ * *****************************************************************************/
 #define __ROS_VERSION__ "3"
 #include <ros/ros.h>
 #include <iostream>
 #include <std_msgs/String.h>
 
 int main(int argc, char** argv) {
-    /* code */
-    std::cout << "/* message */" __ROS_VERSION__ << std::endl;
+    ROS_INFO("Started publisher thread thread");
     ros::init(argc, argv, "basics_writer");
-    std::cout << "/* <> */" << std::endl;
     ros::NodeHandle handler;
 
-    ros::Publisher publisher = handler.advertise<std_msgs::String>("topic", 1000);
+    ros::Publisher publisher =
+        handler.advertise<std_msgs::String>("topic", 1000);
     ros::Rate loop_rate(1);
     int index;
     while (ros::ok()) {
         std_msgs::String msg;
         std::stringstream stream;
 
-        stream << "log " << index++ << " | input message";
+        stream << "{ \"index\": " << index++ \
+            << ", \"name\": " << \
+            ros::this_node::getName() << " }";
         msg.data = stream.str();
-        std::cout << "WRITER " << ros::this_node::getName() << ":: " + msg.data << std::endl;
-
+        std::cout << "WRITER#" + msg.data << std::endl;
         publisher.publish(msg);
-
         ros::spinOnce();
         loop_rate.sleep();
     }
